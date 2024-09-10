f44ef763a48fcc1b7b6ad4016092cd740403d4d3
Noa <noa.houssin@hotmail.com>
2024-09-11 00:06:15 +0200

    connexion Base de données MongoDB et création des models Book et User

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,35 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const Book = require("./models/Books");
 
 const app = express();
 
+mongoose
+  .connect("mongodb+srv://noa:2607@cluster0.c5zvh.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+// app.post("/api/books", (req, res, next) => {});
+app.get("/api/books", (req, res, next) => {
+  Book.find()
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
