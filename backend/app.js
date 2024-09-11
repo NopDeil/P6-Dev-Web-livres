541d32b76e181495ebdfe9b6fcb1ac1984c8edf6
Noa <noa.houssin@hotmail.com>
2024-09-11 23:36:36 +0200

    Ajout du middleware d'authentification et des controleurs login et signup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Book = require("./models/Books");
+
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -9,27 +10,22 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-app.use(express.json());
+  
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
+  
+  app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
-
-// app.post("/api/books", (req, res, next) => {});
-app.get("/api/books", (req, res, next) => {
-  Book.find()
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(400).json({ error }));
-});
+  app.use("/api/auth", userRoutes);
 
 module.exports = app;
