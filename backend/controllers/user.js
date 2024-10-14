fbcf6719e811f3cbdeeba3f5b6d0688c87e4cca8
NopDeil <148153485+NopDeil@users.noreply.github.com>
2024-10-14 21:16:55 +0200

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,7 +20,6 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  console.log(process.env.TOKEN_SECRET)
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
